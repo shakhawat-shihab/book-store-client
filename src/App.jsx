5b574bee259f5705bf5fd1a8a375ed08e4d0ae16
shakhawat <shihabchtg@gmail.com>
2023-10-01 08:46:13 +0600

    main branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  return;
-  <></>;
+  return (
+    <>
+      <h1>This is Main Branch</h1>
+    </>
+  );
 }
 
 export default App;
