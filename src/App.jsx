ca6087cb2c52de2c4b442fee5b9a78d40be6c3f4
shakhawat <shihabchtg@gmail.com>
2023-10-01 08:43:55 +0600

    basic navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import Navbar from "./components/navbar/navbar";
 
-// this is development branch
 function App() {
-  return;
-  <></>;
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,57 @@
+import React, { useContext } from "react";
+import "./navbar.style.css";
+const Navbar = () => {
+  const user = {};
+  return (
+    <nav>
+      <ul className="nav-container">
+        <div className="logo-container">
+          <span>Book Store</span>
+        </div>
+        <div className="navbar-optinos">
+          <li>
+            <a className="active" href="#">
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#services">Services</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          {user?.email ? (
+            <li>
+              <button
+                className="logout-btn"
+                onClick={() => {
+                  console.log("logout......");
+                  logOutUser();
+                }}
+              >
+                Log Out
+              </button>
+            </li>
+          ) : (
+            <li>
+              <button
+                className="login-btn"
+                onClick={() => {
+                  console.log("login......");
+                  logInUser();
+                }}
+              >
+                Log In
+              </button>
+            </li>
+          )}
+        </div>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
